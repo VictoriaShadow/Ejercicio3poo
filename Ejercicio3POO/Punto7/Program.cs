12120946c080d61631988b16a692da14abc9282e
Valen <maidanavalentina2008@gmail.com>
2026-06-30 22:53:16 -0300

    7

--- Ejercicio3POO/Punto7/Program.cs
+++ Ejercicio3POO/Punto7/Program.cs
@@ -6,3 +6,10 @@ Console.WriteLine($"Área: {r1.Area()}");
 Console.WriteLine($"Perímetro: {r1.Perimetro()}");
 Console.WriteLine($"Área: {r2.Area()}");
 Console.WriteLine($"Perímetro: {r2.Perimetro()}");
+
+
+
+
+// Rectangulo r3 = new Rectangulo(-4, 8);
+// Console.WriteLine($"Área: {r3.Area()}");
+// Console.WriteLine($"Perímetro: {r3.Perimetro()}");
